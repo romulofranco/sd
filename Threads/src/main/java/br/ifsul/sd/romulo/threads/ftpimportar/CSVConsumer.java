52749e8a8f52233a1cef80aaf60f00d4c3dce9a7
romulofranco <romulojosefranco@gmail.com>
2019-02-18 10:01:14 -0300

    Threads

--- Threads/src/main/java/br/ifsul/sd/romulo/threads/ftpimportar/CSVConsumer.java
+++ Threads/src/main/java/br/ifsul/sd/romulo/threads/ftpimportar/CSVConsumer.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.ifsul.sd.romulo.threads;
+package br.ifsul.sd.romulo.threads.ftpimportar;
 
+import br.ifsul.sd.romulo.threads.Util;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
--- Threads/src/main/java/br/ifsul/sd/romulo/threads/ftpimportar/CSVResourceHandler.java
+++ Threads/src/main/java/br/ifsul/sd/romulo/threads/ftpimportar/CSVResourceHandler.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.ifsul.sd.romulo.threads;
+package br.ifsul.sd.romulo.threads.ftpimportar;
 
 import java.io.BufferedReader;
 import java.io.File;
