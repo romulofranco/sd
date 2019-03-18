36b826cc9d75c7aaf25015b5196e461106c63f6f
romulofranco <romulojosefranco@gmail.com>
2019-03-17 23:40:30 -0300

    Segundo exemplo de Servidor Multithread com canal de atendimento simultaneo a varios clientes com atendimento de requisições baseado em filas com sincronismo

--- Threads/src/main/java/br/ifsul/sd/romulo/threads/exemplo/servidor/GerenteFilas.java
+++ Threads/src/main/java/br/ifsul/sd/romulo/threads/exemplo/servidor/GerenteFilas.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.ifsul.sd.romulo.threads.exemplo;
+package br.ifsul.sd.romulo.threads.exemplo.servidor;
 
 import java.util.ArrayList;
 import java.util.List;
